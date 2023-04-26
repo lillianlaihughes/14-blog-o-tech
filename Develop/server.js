a45a1c50ba18c8819fa15432e1f1fcdb68c9c772
lillianlaihughes <lillianlaihughes@gmail.com>
2023-04-26 11:48:16 -0400

    updated package.json and added files

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+/* This js file will need to:
+- import all the things (aka require)
+
+- turn on routes
+    this server.js leads to the...
+    > controllers/index.js
+
+    which points to the
+    api folder inside the controllers folder
+
+    specifically to the index.js inside that folder
+    > controllers/api/index.js
+
+    which then points to...
+        api/blogRoutes.js 
+        etc
+
+- turn on connection to database and server
+
+*/
